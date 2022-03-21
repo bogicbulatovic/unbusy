f95a789705bd2423489f9c1f177d9a62d4ec066a
Leo Mrav <udemy13579@protonmail.com>
2022-03-21 12:35:23 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const {
+  createVanillaExtractPlugin
+} = require("@vanilla-extract/next-plugin");
+
+const withVanillaExtract = createVanillaExtractPlugin();
+
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
@@ -5,4 +11,4 @@ const nextConfig = {
   eslint: { dirs: ["src"] }
 };
 
-module.exports = nextConfig;
+module.exports = withVanillaExtract(nextConfig);
