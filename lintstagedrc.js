e21f5fa4e76d91bc7bfedcee4c0a8fc2647a654b
pindjur <64326054+pindjur@users.noreply.github.com>
2022-12-13 22:56:00 +0100

    adjust lintstaged script to avoid ignored

--- lintstagedrc.js
+++ lintstagedrc.js
@@ -1,10 +1,26 @@
 const path = require("path");
+const { ESLint } = require("eslint");
 
-const buildEslintCommand = filenames =>
-  `eslint --max-warnings=0 ${filenames
+const eslint = new ESLint();
+
+const removeIgnoredFiles = async files => {
+  const isIgnored = await global.Promise.all(
+    files.map(file => {
+      return eslint.isPathIgnored(file);
+    })
+  );
+  const filteredFiles = files.filter((_, i) => !isIgnored[i]);
+
+  return filteredFiles;
+};
+
+const buildEslintCommand = async filenames => {
+  const filesToLint = await removeIgnoredFiles(filenames);
+
+  return `eslint --max-warnings=0 ${filesToLint
     .map(f => path.relative(process.cwd(), f))
     .join(" ")}`;
-
+};
 const buildTypeCheckCommand = () => "tsc --noEmit --incremental";
 
 module.exports = {
