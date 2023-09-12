5ddb451cadb3c6d5a95f3d1df1545b0d78035796
pindjur <64326054+pindjur@users.noreply.github.com>
2023-09-12 19:12:03 +0200

    create normalizeGeo script

---
+++ scripts/normalizeGeo.mjs
@@ -0,0 +1,41 @@
+import { readFileSync, readdirSync, writeFileSync } from "node:fs";
+
+const normalizeArr = arr => {
+  if (
+    arr.length === 2 &&
+    typeof arr[0] === "number" &&
+    typeof arr[1] === "number"
+  ) {
+    const arr0 = arr[0];
+    const arr1 = arr[1];
+    if (arr0 > arr1) {
+      console.log("swapping", arr);
+      arr[0] = arr1;
+      arr[1] = arr0;
+    }
+  }
+};
+
+const forEachArr = (obj, cb) => {
+  for (const prop in obj) {
+    const val = obj[prop];
+    if (Array.isArray(val)) {
+      cb(val);
+    }
+    if (typeof val === "object") {
+      forEachArr(val, cb);
+    }
+  }
+};
+
+const dirPath = "./src/data/bus-lines-json/";
+const files = readdirSync(dirPath);
+
+for (const filename of files) {
+  const file = readFileSync(dirPath + filename);
+  const obj = JSON.parse(file);
+
+  forEachArr(obj, normalizeArr);
+
+  writeFileSync(dirPath + filename, JSON.stringify(obj));
+}
