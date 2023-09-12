10314960017ea9436f9e6691fbb93ea59dae5695
pindjur <64326054+pindjur@users.noreply.github.com>
2023-09-12 15:24:37 +0200

    set center to podgiorica,remove example popup,marker

--- src/components/page/index/Map/Map.tsx
+++ src/components/page/index/Map/Map.tsx
@@ -1,11 +1,7 @@
 import React from "react";
-import {
-  MapContainer,
-  TileLayer,
-  Marker,
-  Popup
-} from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import * as s from "./styles";
+import { PodgoricaLatLng, zoom } from "./helpers";
 
 const Map: React.FC = () => {
   if (typeof window === "undefined") {
@@ -14,19 +10,14 @@ const Map: React.FC = () => {
 
   return (
     <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
+      center={PodgoricaLatLng}
+      zoom={zoom}
       className={s.root}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
     </MapContainer>
   );
 };
---
+++ src/components/page/index/Map/helpers.ts
@@ -0,0 +1,9 @@
+import { LatLngExpression } from "leaflet";
+
+const PodgoricaLatLng: LatLngExpression = [
+  42.43736898801881, 19.26391931207956
+];
+
+const zoom = 14;
+
+export { PodgoricaLatLng, zoom };
