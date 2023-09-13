b4c5fd576518bda57f8dfc03c171ad4375d466a0
pindjur <64326054+pindjur@users.noreply.github.com>
2023-09-13 17:21:25 +0200

    create and run genBusLinesMapping script

---
+++ scripts/genBusLinesMapping.mjs
@@ -0,0 +1,20 @@
+// generate mappings for use for dynamic import from mock api request
+// example : /api/bus-lines?85 we fetch import(mapping[85].json)
+
+import { writeFileSync, readFileSync } from "node:fs";
+import { dirPath, forEachBusLineFile } from "./utils.mjs";
+
+const obj = {};
+
+forEachBusLineFile(filename => {
+  const file = readFileSync(dirPath + filename);
+  const fileObj = JSON.parse(file);
+
+  obj[fileObj.features[0].properties.id] =
+    fileObj.features[0].properties.name;
+});
+
+writeFileSync(
+  "./scripts/dist/busLinesMapping.json",
+  JSON.stringify(obj, null, 4)
+);
