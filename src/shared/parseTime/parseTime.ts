e26d2f44dced731bd2106cb84e52d783bcce0bf4
pindjur <64326054+pindjur@users.noreply.github.com>
2023-09-15 18:02:11 +0200

    create parse time util

---
+++ src/shared/parseTime/parseTime.ts
@@ -0,0 +1,31 @@
+import { ParseTypeResponse as Response } from "./types";
+
+const parseTime = (time: string): Response => {
+  const splitted = time.split(":");
+
+  let hours: string | number = splitted[0];
+  let minutes: string | number = splitted[1];
+  let seconds: string | number = splitted[2];
+
+  if (hours.charAt(0) === "0") {
+    hours = hours.charAt(1);
+  }
+
+  hours = Number(hours);
+
+  if (minutes.charAt(0) === "0") {
+    minutes = minutes.charAt(1);
+  }
+
+  minutes = Number(minutes);
+
+  if (seconds.charAt(0) === "0") {
+    seconds = seconds.charAt(1);
+  }
+
+  seconds = Number(seconds);
+
+  return { hours, minutes, seconds };
+};
+
+export { parseTime };
---
+++ src/shared/parseTime/types.ts
@@ -0,0 +1,5 @@
+export type ParseTypeResponse = {
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
