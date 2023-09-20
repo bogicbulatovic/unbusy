93ba36516a495cc3277b217df5e3d3cab92929c2
pindjur <64326054+pindjur@users.noreply.github.com>
2023-09-20 16:44:33 +0200

    stringify time with 0 prefix if needed

--- src/shared/time/stringifyTime.ts
+++ src/shared/time/stringifyTime.ts
@@ -1,11 +1,16 @@
 import { Time } from "./types";
 
 const stringifyTime = (time: Time) => {
+  const minutes =
+    time.minutes < 10 ? `0${time.minutes}` : time.minutes;
+
+  const hours = time.hours < 10 ? `0${time.hours}` : time.hours;
+
   if (time.seconds !== undefined) {
-    return `${time.hours}:${time.minutes}:${time.seconds}`;
+    return `${hours}:${minutes}:${time.seconds}`;
   }
 
-  return `${time.hours}:${time.minutes}`;
+  return `${hours}:${minutes}`;
 };
 
 export { stringifyTime };
