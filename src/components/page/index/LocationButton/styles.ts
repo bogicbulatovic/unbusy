7b044045ed478c9fa17c7347b6748164f7b9cf40
pindjur <64326054+pindjur@users.noreply.github.com>
2023-09-20 17:47:29 +0200

    create LocationButton

---
+++ src/components/page/index/LocationButton/styles.ts
@@ -0,0 +1,19 @@
+import { styled } from "../../../../styled";
+import { ButtonBase } from "../../../shared/ButtonBase/ButtonBase";
+
+const Root = styled(ButtonBase, {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "absolute",
+  zIndex: 1000,
+  left: "5vw",
+  bottom: "5vw",
+  backgroundColor: "white",
+  borderRadius: "50%",
+  border: "2px solid rgba(0, 0, 0, 0.2)",
+  fontSize: 40,
+  padding: 10
+});
+
+export { Root };
